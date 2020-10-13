e2cb0ee9ede997b372f114364699a5433320592d
zidol <ajh3166@naver.com>
2020-10-14 00:24:58 +0900

    add jpql example entity

---
+++ src/main/java/jpal/JpaMain.java
@@ -0,0 +1,30 @@
+package jpal;
+
+
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello"); //애플리케이션 처음 실행 될때
+
+        EntityManager em = emf.createEntityManager(); // 사용자 요청시
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+            Member member = new Member();
+            member.setUsername("member1");
+            em.persist(member);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
