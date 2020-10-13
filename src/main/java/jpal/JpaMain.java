6cfaaa0dbee1fb37f00d6feb2efebab47917b469
zidol <ajh3166@naver.com>
2020-10-14 00:41:48 +0900

    jpql 기본 문법

--- src/main/java/jpal/JpaMain.java
+++ src/main/java/jpal/JpaMain.java
@@ -2,10 +2,8 @@ package jpal;
 
 
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -18,7 +16,23 @@ public class JpaMain {
         try {
             Member member = new Member();
             member.setUsername("member1");
+            member.setAge(10);
             em.persist(member);
+
+//            TypedQuery<Member> query = em.createQuery("select m from Member m where m.id = 10", Member.class);
+//            List<Member> resultList = query.getResultList();      //리스트 반환, 데이터 없을때 빈 리스트 반환
+//            Member singleResult = query.getSingleResult();  // 싱글 반환  //경과가 정확히 하나만 있어야함, 2개 이상 , 없으면 예외 발생
+
+//            TypedQuery<Member> query = em.createQuery("select m from Member m where m.username = :username", Member.class);
+//            query.setParameter("username", "member1");
+//            Member singleResult = query.getSingleResult();
+            //  이름 기반 파라미터 권장
+            Member singleResult = em.createQuery("select m from Member m where m.username = :username", Member.class)
+                    .setParameter("username", "member1")
+                    .getSingleResult();
+            System.out.println("singleResult.getUsername() = " + singleResult.getUsername());
+
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
